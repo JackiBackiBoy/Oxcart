90bcaad62d5ed2abd86bbf792b4da78085de3835
JackiBackiBoy <TheGameTail@gmail.com>
2020-10-03 18:40:18 +0200

    Completed Assimp implementation

--- OpenGL3D/src/data/Mesh.cpp
+++ OpenGL3D/src/data/Mesh.cpp
@@ -7,10 +7,14 @@ Mesh::Mesh(const std::vector<Vertex>& someVertices, const std::vector<unsigned i
 	glGenVertexArrays(1, &myVAO);
 	glGenBuffers(1, &myVBO);
 	glGenBuffers(1, &myEBO);
-
-	// VAO
-	// Vertex positions
 	glBindVertexArray(myVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, myVBO);
+	glBufferData(GL_ARRAY_BUFFER, myVertices.size() * sizeof(Vertex), &myVertices[0], GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, myEBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, myIndices.size() * sizeof(unsigned int), &myIndices[0], GL_STATIC_DRAW);
+
+	// Vertex position
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
 
@@ -21,14 +25,6 @@ Mesh::Mesh(const std::vector<Vertex>& someVertices, const std::vector<unsigned i
 	// Texture coords
 	glEnableVertexAttribArray(2);
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, textureCoords));
-
-	// VBO
-	glBindBuffer(GL_ARRAY_BUFFER, myVBO);
-	glBufferData(GL_ARRAY_BUFFER, myVertices.size() * sizeof(Vertex), &myVertices[0], GL_STATIC_DRAW);
-
-	// EBO
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, myEBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, myIndices.size() * sizeof(unsigned int), &myIndices[0], GL_STATIC_DRAW);
 
 	glBindVertexArray(0);
 }
@@ -54,12 +50,12 @@ void Mesh::Render(Shader& aShader)
 			tempNumber = std::to_string(tempSpecularNumber++);
 		}
 
-		glUniform1i(glGetUniformLocation(aShader.GetID(), ("material." +  tempName).c_str()), i);
+		glUniform1ui(glGetUniformLocation(aShader.GetID(), ("material." +  tempName).c_str()), i);
 		glBindTexture(GL_TEXTURE_2D, myTextures[i].ID);
 	}
 
 	glBindVertexArray(myVAO);
-	glDrawElements(GL_TRIANGLES, myIndices.size(), GL_UNSIGNED_INT, (void*)0);
+	glDrawElements(GL_TRIANGLES, myIndices.size(), GL_UNSIGNED_INT, 0);
 	glBindVertexArray(0);
 
 	glActiveTexture(GL_TEXTURE0);
