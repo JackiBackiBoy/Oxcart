6b358b6b0ab989d97ffdd07daaaaea237c5f6b5c
JackiBackiBoy <TheGameTail@gmail.com>
2020-10-10 15:54:13 +0200

    UI code cleanup

--- OpenGL3D/src/ui/UIImage.cpp
+++ OpenGL3D/src/ui/UIImage.cpp
@@ -2,53 +2,14 @@
 
 UIImage::UIImage(const Texture& aTexture, const Vector3D& aPosition) : myTexture(aTexture), myVAO(0), myVBO(0), myEBO(0), UIElement(aPosition)
 {
-	float tempVertices[4][4] =
-	{
-		myPosition.x,  myPosition.y, 0.0f, 1.0f, // top left
-		myPosition.x, (float)myTexture.GetHeight(), 0.0,  0.0f, // bottom left
-		(float)myTexture.GetWidth(), (float)myTexture.GetHeight(), 1.0,  0.0f, // bottom right
-		(float)myTexture.GetWidth(), myPosition.x, 1.0f, 1.0f, // top right
-	};
-
-	unsigned int tempIndices[6] = { 0, 1, 2, 0, 2, 3 };
-
 	glGenVertexArrays(1, &myVAO);
 	glGenBuffers(1, &myVBO);
 	glGenBuffers(1, &myEBO);
-
-	// Element Buffer Object (EBO)
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, myEBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), tempIndices, GL_STATIC_DRAW);
-
-	// Vertex Array Object (VAO)
-	glBindVertexArray(myVAO);
-	glBindBuffer(GL_VERTEX_ARRAY, myVAO);
-
-	// Vertex Buffer Object (VBO)
-	glBindBuffer(GL_ARRAY_BUFFER, myVBO);
-	glBufferData(GL_ARRAY_BUFFER, 24 * sizeof(float), tempVertices, GL_STATIC_DRAW);
-
-	// Position and texture coordinates
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 4, GL_FLOAT, false, sizeof(float) * 4, 0);
 }
 
 void UIImage::Render(Window& aWindow)
 {
-
-
-	//// A Quad
-	//float tempVertices[6][4] =
-	//{
-	//    myPosition.x,  myPosition.y, 0.0f, 1.0f, // top left
-	//    myPosition.x, myTexture.GetHeight(), 0.0,  0.0f, // bottom left
-	//	myTexture.GetWidth(), myTexture.GetHeight(), 1.0,  0.0f, // bottom right
-
-	//    myPosition.x, myPosition.y, 0.0f, 1.0f, // top left
-	//	myTexture.GetWidth(), myTexture.GetHeight(), 1.0,  0.0f, // bottom right
-	//	myTexture.GetWidth(), myPosition.x, 1.0f, 1.0f, // top right
-	//};
-
+	// A Quad
 	float tempVertices[4][4] =
 	{
 		myPosition.x,  myPosition.y, 0.0f, 1.0f, // top left
